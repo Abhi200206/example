f4f9b6d6b579ba129df548562e1e0e6a7e092ef8
Abhi200206 <120104020+Abhi200206@users.noreply.github.com>
2023-02-22 22:20:17 +0530

    commit

--- hi.java
+++ hi.java
@@ -3,5 +3,10 @@ class hi{
     public static void main(String args)
     {
         System.out.println("hi");
+        void hi(int i){
+          System.out.println("hi"+""+i);  
+        }
+        hi h=new hi();
+        h.hi(5);
     }
 }
